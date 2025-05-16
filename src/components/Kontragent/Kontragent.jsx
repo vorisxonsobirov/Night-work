c9b5a6c3aab974fb0d4e295ff04fbe537b520c7a
Vorisxon <vorisxon.me@gmail.com>
2025-05-16 10:09:24 +0500

    second cloud

--- src/components/Kontragent/Kontragent.jsx
+++ src/components/Kontragent/Kontragent.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Kontragent = () => {
-  return (
-    <div className='Kontragent'>
-      <h1>Kontragent</h1>
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default Kontragent
+  useEffect(() => {
+    // Получение данных при загрузке компонента
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Ошибка при загрузке:', error));
+  }, []);
+
+  return (
+    <div className="kontragentlar">
+      <h2>Список контрагентов</h2>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Компания</th>
+            <th>Email</th>
+            <th>Телефон</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.company.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Kontragent;
